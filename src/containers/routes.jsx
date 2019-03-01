0376748a87012b7657015f47de54b00bc1024750
christopherhafke <chafke@umich.edu>
2019-02-28 19:15:32 -0500

    Added framework for routing

---
+++ src/containers/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Home = () => null; // TODO: replace with real homepage
+
+export const RouterGroup = (
+    <Router>
+        <React.Fragment>
+            <Route exact path="/" component={Home}/>
+        </React.Fragment>
+    </Router>  
+);
