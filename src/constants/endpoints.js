c251a634e4399aaefff1ed26676a89350d0e6b8f
Eashwar Mohan <eashwar@umich.edu>
2019-03-24 20:00:15 -0400

    add actions and action creators for getClassById

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,6 @@
 // Endpoints
 
 export const endpoints = Object.freeze({
-    GET_REVIEW_BY_ID: '/review/' // placeholder, of course
+    GET_REVIEW_BY_ID: '/review/', // placeholder, probably
+    GET_CLASS_BY_ID: '/class/'
 });
