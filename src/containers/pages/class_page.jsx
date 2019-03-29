1ce6af213cdaec5071f64f118894d76239e9d766
Eashwar Mohan <eashwar@umich.edu>
2019-03-28 21:39:15 -0400

    initial work on class page

--- src/containers/pages/class_page.jsx
+++ src/containers/pages/class_page.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ClassActions } from '../../actions/class_actions.js';
+import styled from 'styled-components';
+
+
+const ClassName = styled.h1`
+    font-size: 30vh;
+`
 
 class ClassPage extends React.Component {
     constructor() {
         super();
+        this.getClassId = this.getClassId.bind(this);
     }
 
     getClassId() {
-        pathName = this.props.location.pathname;
-        return parseInt(pathName.substring(pathName.lastIndexOf('/'))); // extract the id, which is after the final forwardslash of the path
+        const pathName = this.props.location.pathname;
+        return parseInt(pathName.substring(pathName.lastIndexOf('/') + 1)); // extract the id, which is after the final forwardslash of the path
     }
 
     componentDidMount() {
-        this.props.getClassById(getClassId());
+        this.props.getClassById(this.getClassId());
     }
 
     render() {
         return (
-            <span>Placeholder</span>
+            <div className='class-page'>
+            <ClassName>{this.props.classData ? this.props.classData.name : 'NOTHING YET'}</ClassName>
+            </div>
         )
     }
 }
 
-const mapStateToProps = state => ({
-    classId: state.searchClassesByName.id,
-    classData: state.getClassById.classData,
-    reviews: state.getClassById.classData.reviews
+const mapStateToProps = state =>({
+    classData: (state.classReducers.getClassById.classData) ? state.classReducers.getClassById.classData : null,
+    reviews: (state.classReducers.getClassById.classData) ? state.classReducers.getClassById.classData.reviews : null
 })
 
-const mapDispatchToProps = () => ({
-    getClassById: ClassActions.getClassByIdRequest
-    // (Request) action creators go here
+const mapDispatchToProps = dispatch => ({
+    getClassById: id => dispatch(ClassActions.getClassByIdRequest(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ClassPage);
--- src/containers/routes.jsx
+++ src/containers/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ClassPage from './pages/class_page.jsx';
 
 const Home = () => null; // TODO: replace with real homepage
+
 
 export const RouterGroup = () => (
     <Router>
         <React.Fragment>
             <Route exact path="/" component={Home}/>
+            <Route path="/class" component={ClassPage}/>
         </React.Fragment>
     </Router>  
 );
