4cbfeecb43839995846d50377ab1cab4600fe829
Eashwar Mohan <eashwar@umich.edu>
2019-03-31 18:40:54 -0400

    switch to dummy data instead of backend call

--- src/sagas/class_sagas.js
+++ src/sagas/class_sagas.js
@@ -6,13 +6,13 @@ import { call, put, takeLatest, actionChannel } from 'redux-saga/effects';
 import { ClassActions } from '../actions/class_actions'
 
 function* getClassById(action) {
-    // const { json, error } = { json: { 
-    //                                     reviews: [{ name: "test1" }, {name: "test2"}],
-    //                                     name: "TEST 420",
-    //                                     id: action.id
-    //                                 }, error: null}
+    const { json, error } = { json: { 
+                                        reviews: [{ name: "test1" }, {name: "test2"}],
+                                        name: "EECS 281",
+                                        id: action.id
+                                    }, error: null}
                                 
-    const { json, error } = yield call(Get, endpoints.GET_CLASS_BY_ID, null, action.id); // Object destructuring
+    // const { json, error } = yield call(Get, endpoints.GET_CLASS_BY_ID, null, action.id); // Object destructuring
     if (error) {
         // Dispatch redux action
         yield put(ClassActions.getClassByIdFailure(error));
