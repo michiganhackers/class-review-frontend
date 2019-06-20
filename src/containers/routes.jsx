07bed8c2ed41a9915bc6e5e7c6934394ba9a5eb9
Brian Chan <brchan@umich.edu>
2019-06-19 23:39:44 -0400

    put dummy values in review_form.jsx and imported from routes.jsx to display values

--- src/containers/routes.jsx
+++ src/containers/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginPage from './pages/login_page.jsx'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import CoursePage from './pages/course_page.jsx';
+import ReviewForm from './pages/review_form.jsx';
 
 const Home = () => null; // TODO: replace with real homepage
 
@@ -11,6 +12,7 @@ export const RouterGroup = () => (
         <React.Fragment>
             <Route exact path="/" component={Home}/>
             <Route path="/course" component={CoursePage}/>
+            <Route path="/yote" component={ReviewForm}/>
             <Route exact path="/login" component={LoginPage}/>
         </React.Fragment>
     </Router>  
