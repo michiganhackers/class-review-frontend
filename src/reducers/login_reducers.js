8d7e8225cb08d63c2ae3fd09e84018d7460893ec
Eashwar Mohan <eashwar@umich.edu>
2019-06-23 15:00:26 -0700

    add reducers for login

---
+++ src/reducers/login_reducers.js
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux';
+import { loginTypes } from './../constants/action_types';
+
+function setLoginTokens(state = {}, action) {
+    switch (action.type) {
+        case loginTypes.SET_LOGIN_TOKENS: 
+            return {
+                ...state,
+                error: null,
+                tokens: action.tokens
+            }
+        default:
+            return state
+    }
+}
+
+function profileInfo(state = {}, action){
+    switch (action.type) {
+        case loginTypes.SET_PROFILE_INFO:
+            return {
+                ...state,
+                error: null,
+                profile: action.profile
+            }
+        case loginTypes.CLEAR_PROFILE_INFO:
+            return {
+                ...state,
+                error: null,
+                profile: null
+            }
+        default:
+            return state
+    }
+}
+
+
+const loginReducers = combineReducers({
+    setLoginTokens,
+    profileInfo
+})
+
+export default loginReducers;
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,13 @@
 // Reducers
 import reviewReducers from './review_reducers.js';
 import courseReducers from './course_reducers.js';
+import loginReducers from './login_reducers.js';
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
     reviewReducers,
-    courseReducers
+    courseReducers,
+    loginReducers
 })
 
 export default reducer;
