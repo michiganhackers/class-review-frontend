8f2b39ea9eeab37d4e273827413391cc9749d9a6
Eashwar Mohan <eashwar@umich.edu>
2019-07-23 21:38:24 -0700

    react-redux stuff

--- src/global_components/searchbar.jsx
+++ src/global_components/searchbar.jsx
@@ -9,85 +9,95 @@ import styled from 'styled-components';
 import MediaQuery from 'react-responsive';
 import Collapse from 'react-bootstrap/Collapse';
 import Button from 'react-bootstrap/Button';
+import { SearchActions } from '../actions/search_actions.js'
 
-class SearchBar extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      navOpen: false,
-    };
-    this.drawSearchResults = this.drawSearchResults.bind(this);
-  }
+class SearchBar extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            navOpen: false,
+        };
+        this.drawSearchResults = this.drawSearchResults.bind(this);
+        this.requestSearchResults = this.requestSearchResults.bind(this);
+    }
 
-  drawSearchResults(){
-    const results = [];//[{title: 'EECS 280', id: '12345'}, {title: 'EECS 281', id: '23456'}, {title: 'STATS 250', id: '34567'}];
-    const searchResults = results.map(course => {
-      return (<Dropdown.Item key={course.id.toString()} href={'/course/' + course.id}>{course.title}</Dropdown.Item>);
-    });
-    const Results = styled(Dropdown.Menu)`
-      position: absolute;
-      width: 100%;
-      margin-top: 0px;
-      :empty {
-        display: none;
-      }
-    `;
+    requestSearchResults(event) {
+        this.props.getSearchResults(event.target.value);
+    }
 
-    return(
-      <Results show>
-        {searchResults}
-      </Results>
-    );
-  }
+    drawSearchResults() {
+        let searchResults = [];
+        if (this.props.results){
+            searchResults = this.props.results.map(course => {
+                return (<Dropdown.Item key={course.id.toString()} href={'/course/' + course.id}>{course.title}</Dropdown.Item>);
+            });
+        }
+        const Results = styled(Dropdown.Menu)`
+            position: absolute;
+            width: 100%;
+            margin-top: 0px;
+            :empty {
+                display: none;
+            }
+        `;
 
-  render() {
-    const logoUrl = process.env.PUBLIC_URL + '/logo.gif';
-    return (
-      <div>
-        <Navbar bg='light' variant='light' className='justify-content-around' >
-          <Navbar.Brand href='/'>
-            <img
-              src={logoUrl}
-              width='30'
-              height='30'
-              className='d-inline-block align-top'
-              alt='React Bootstrap logo'
-            />
-          </Navbar.Brand>
-          <MediaQuery minDeviceWidth={992}>
-            <Form className='position-relative' inline>
-              <FormControl type='text' placeholder='Search for a class' className='w-100' />
-              {this.drawSearchResults()}
-            </Form>
-            <LoginButton/>
-          </MediaQuery>
-          <MediaQuery maxDeviceWidth={991}>
-            <Button
-              className='navbar-toggler-icon px-4 py-4'
-              onClick={() => this.setState({navOpen: !this.state.navOpen})}
-              aria-controls='collapsedNavbarContent' 
-              aria-expanded={this.state.navOpen}
-            />
-          </MediaQuery>
-        </Navbar>
-        <MediaQuery maxDeviceWidth={991}>
-          <Collapse in={this.state.navOpen}>
-            <div className='bg-light' id='collapsedNavbarContent'>
-              <Form className='position-relative' inline>
-                <FormControl type='text' placeholder='Search for a class' className='w-100' />
-                {this.drawSearchResults()}
-              </Form>
-              <LoginButton/>
+        return(
+            <Results show>
+                {searchResults}
+            </Results>
+        );
+    }
+
+    render() {
+        const logoUrl = process.env.PUBLIC_URL + '/logo.gif';
+        return (
+            <div>
+                <Navbar bg='light' variant='light' className='justify-content-around' >
+                    <Navbar.Brand href='/'>
+                        <img
+                            src={logoUrl}
+                            width='30'
+                            height='30'
+                            className='d-inline-block align-top'
+                            alt='React Bootstrap logo'
+                        />
+                    </Navbar.Brand>
+                    <MediaQuery minDeviceWidth={992}>
+                        <Form className='position-relative' inline>
+                            <FormControl type='text' placeholder='Search for a class' className='w-100' onChange={this.requestSearchResults}/>
+                            {this.drawSearchResults()}
+                        </Form>
+                        <LoginButton/>
+                    </MediaQuery>
+                    <MediaQuery maxDeviceWidth={991}>
+                        <Button
+                            className='navbar-toggler-icon px-4 py-4'
+                            onClick={() => this.setState({navOpen: !this.state.navOpen})}
+                            aria-controls='collapsedNavbarContent' 
+                            aria-expanded={this.state.navOpen}
+                        />
+                    </MediaQuery>
+                </Navbar>
+                <MediaQuery maxDeviceWidth={991}>
+                    <Collapse in={this.state.navOpen}>
+                        <div className='bg-light' id='collapsedNavbarContent'>
+                            <Form className='position-relative' inline>
+                                <FormControl type='text' placeholder='Search for a class' className='w-100' />
+                                {this.drawSearchResults()}
+                            </Form>
+                            <LoginButton/>
+                        </div>
+                    </Collapse>
+                </MediaQuery>
             </div>
-          </Collapse>
-        </MediaQuery>
-      </div>
-      );
-  }
+            );
+    }
 }
 const mapStateToProps = state => ({
+    results: (state.searchReducers.getSearchResults.results)
 })
 
 const mapDispatchToProps = dispatch => ({
-})  
+    getSearchResults: query => dispatch(SearchActions.getSearchResultsRequest(query))
+})    
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
